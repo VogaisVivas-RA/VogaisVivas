24a3421fcd9b0c07fee04b3cf50004a006c5929d
FelipeDias77 <felipediassantana@gmail.com>
2026-06-18 00:26:27 -0300

    Refinamento da Tela Inicial e Padronização da Letra "A"
    - Foi realizado o refinamento da tela Inicial
    - Adequação da letra A para 3D

---
+++ Assets/MudaCena.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MudaCena : MonoBehaviour
+{
+    public void IrParaMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
